2a9f6ff94f782af92be658d65d4269e5193e0955
milandhimannn <milandhiman95@gmail.com>
2023-05-01 18:19:44 +0530

    Initial Commit

---
+++ AddDatatoDatabase.py
@@ -0,0 +1,45 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://face-attendance-6d18e-default-rtdb.firebaseio.com/"
+})
+
+ref =db.reference('Students')
+
+data = {
+    "1504":
+        {
+            "Name": "Akash Kashyap",
+            "Course": "Btech-IT",
+            "Starting_Year": 2020,
+            "total_attendance":5,
+            "year": 6,
+            "last_attendance_time": "01-05-2023  17:00:30",
+        },
+    "1522":
+        {
+            "Name":"Milan Dhiman",
+            "Course": "Btech-IT",
+            "Starting_Year": 2020,
+            "year": 6,
+            "total_attendance": 5,
+            "last_attendance_time": "01-05-2023  17:00:30",
+
+        },
+    "1560":
+        {
+            "Name":"Gagandeep Singh",
+            "Course": "Btech-IT",
+            "Starting_Year": 2020,
+            "year":6,
+            "total_attendance": 5,
+            "last_attendance_time": "01-05-2023  17:00:30",
+
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
