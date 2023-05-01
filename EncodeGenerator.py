59752d3964d962d73bc3ee8a5ce6ff86dc9fa5c0
Milan Dhiman <123298995+milandhimannn@users.noreply.github.com>
2023-05-01 18:32:14 +0530

    EncodeGenerator.py

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -9,8 +9,8 @@ from firebase_admin import storage
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
-    'databaseURL': "https://face-attendance-6d18e-default-rtdb.firebaseio.com/",
-    'storageBucket':"face-attendance-6d18e.appspot.com"
+    'databaseURL': "",
+    'storageBucket':""
 })
 
 
