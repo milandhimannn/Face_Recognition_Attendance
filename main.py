1fc1d6cdb9eae6a268019ddc849b1cd407e4c9a7
Milan Dhiman <123298995+milandhimannn@users.noreply.github.com>
2023-05-01 18:32:40 +0530

    main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from datetime import datetime
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
-    'databaseURL': "https://face-attendance-6d18e-default-rtdb.firebaseio.com/",
-    'storageBucket':"face-attendance-6d18e.appspot.com"
+    'databaseURL': "",
+    'storageBucket':""
 })
 
 bucket = storage.bucket()
